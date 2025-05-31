807eec32f00748242b84a223286aac8a0a0c6a6b
dannyInfras <choecl263@gmail.com>
2025-05-31 23:22:02 +0700

    fix: update node version for next.js deps

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,20 +13,46 @@ provider "digitalocean" {
   token = var.do_token
 }
 
+# --------------------------------------------------------------------------------------
+# 1) Conditionally create a new SSH Key (only if var.create_ssh_key == true)
+# --------------------------------------------------------------------------------------
 resource "digitalocean_ssh_key" "deployer" {
+  count      = var.create_ssh_key ? 1 : 0
   name       = "${var.droplet_name}-key"
   public_key = var.ssh_public_key
 }
 
+# --------------------------------------------------------------------------------------
+# 2) If create_ssh_key == false, look up an existing SSH key by name
+# --------------------------------------------------------------------------------------
+data "digitalocean_ssh_key" "deployer" {
+  count = var.create_ssh_key ? 0 : 1
+  name  = "${var.droplet_name}-key"
+}
+
+# --------------------------------------------------------------------------------------
+# 3) Create the Droplet, referencing whichever key (resource or data) is active
+# --------------------------------------------------------------------------------------
 resource "digitalocean_droplet" "web" {
   name   = var.droplet_name
   region = var.region
   size   = var.size
   image  = var.image
 
-  ssh_keys = [digitalocean_ssh_key.deployer.fingerprint]
+  # Use the newly created key’s fingerprint if count=1, else use the existing key’s fingerprint:
+  ssh_keys = [
+    var.create_ssh_key 
+      ? digitalocean_ssh_key.deployer[0].fingerprint 
+      : data.digitalocean_ssh_key.deployer[0].fingerprint
+  ]
 }
 
+# --------------------------------------------------------------------------------------
+# 4) Allocate a Floating IP in the same region and attach it to the Droplet
+#    (Terraform will only run this block the very first time, because our CI only calls
+#     'terraform apply' if the Droplet did not previously exist.)
+# --------------------------------------------------------------------------------------
 resource "digitalocean_floating_ip" "ip" {
   droplet_id = digitalocean_droplet.web.id
+  region     = var.region
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,16 @@
 variable "do_token" {
-  description = "DigitalOcean API token"
+  description = "DigitalOcean API token (must have Read+Write on Droplets, SSH Keys, Floating IPs)"
   type        = string
   sensitive   = true
 }
 
 variable "droplet_name" {
-  description = "Name of the DigitalOcean Droplet"
+  description = "Name of the DigitalOcean Droplet (also used as the SSH‐Key name)"
   type        = string
 }
 
 variable "region" {
-  description = "DigitalOcean region (e.g. nyc3, sfo2)"
+  description = "DigitalOcean region (e.g. nyc3, sfo2, etc.)"
   type        = string
   default     = "nyc3"
 }
@@ -22,12 +22,18 @@ variable "size" {
 }
 
 variable "image" {
-  description = "Droplet image slug (e.g. ubuntu-22-04-x64)"
+  description = "Droplet base image slug (e.g. ubuntu-22-04-x64)"
   type        = string
   default     = "ubuntu-22-04-x64"
 }
 
 variable "ssh_public_key" {
-  description = "Public SSH key to install on the Droplet"
+  description = "The public SSH key (in OpenSSH format) to use for root access"
   type        = string
+}
+
+variable "create_ssh_key" {
+  description = "Whether to create a new SSH Key (true) or use an existing one (false)."
+  type        = bool
+  default     = true
 }
