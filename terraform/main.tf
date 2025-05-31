6c43b4463ad8a5f43b7ff02aa689a90c7aea0da5
dannyInfras <choecl263@gmail.com>
2025-05-31 22:46:20 +0700

    chore: Update GitHub Actions workflow to install Terraform 1.5.7

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
@@ -11,13 +13,11 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-# Upload the SSH Key
 resource "digitalocean_ssh_key" "deployer" {
   name       = "${var.droplet_name}-key"
   public_key = var.ssh_public_key
 }
 
-# Create the Droplet
 resource "digitalocean_droplet" "web" {
   name   = var.droplet_name
   region = var.region
@@ -27,12 +27,6 @@ resource "digitalocean_droplet" "web" {
   ssh_keys = [digitalocean_ssh_key.deployer.fingerprint]
 }
 
-# Associate a Floating IP so that the IP remains stable across re-creations
 resource "digitalocean_floating_ip" "ip" {
   droplet_id = digitalocean_droplet.web.id
-}
-
-output "droplet_ip" {
-  description = "The public IPv4 address of the Droplet (Floating IP)"
-  value       = digitalocean_floating_ip.ip.ip_address
 }
